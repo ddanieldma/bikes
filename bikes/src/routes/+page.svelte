c1e86f901668270fb364613e4371327249d30a36
ddanieldma <danielalmemiranda@gmail.com>
2025-05-15 12:39:18 -0300

    adds cambridge bike lanes data

--- bikes/src/routes/+page.svelte
+++ bikes/src/routes/+page.svelte
@@ -21,30 +21,40 @@
 
         await new Promise(resolve => map.on("load", resolve));
 
-        try {
-            const response = await fetch("/data/Existing_Bike_Network_2022.geojson"); // Adjust the path if your file name is different
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const bikeLaneData = await response.json();
-
-            console.log("bikelanedata", bikeLaneData)
-
-            map.addSource("boston_route", {
-                type: "geojson",
-                data: bikeLaneData,
-            });
-        } catch (error) {
-            console.error("Error loading local bike lane data:", error);
-        }
+        // Adding Boston bike lanes data
+        const responseBoston = await fetch("/data/Existing_Bike_Network_2022.geojson"); // Adjust the path if your file name is different
+        const bikeLaneDataBoston = await responseBoston.json();
+        map.addSource("boston_route", {
+            type: "geojson",
+            data: bikeLaneDataBoston,
+        });
+        
+        // Adding Cambridge bike lanes data
+        const responseCambridge = await fetch("/data/cambridge_bike.geojson"); // Adjust the path if your file name is different
+        const bikeLaneDataCambridge = await responseCambridge.json();
+        map.addSource("cambridge_route", {
+            type: "geojson",
+            data: bikeLaneDataCambridge,
+        });
 
         map.addLayer({
-            id: "bike_lanes_layer",
+            id: "bike_lanes_layer_boston",
             type: "line", // one of the supported layer types, e.g. line, circle, etc.
             source: "boston_route", // The id we specified in `addSource()`
             paint: {
-                "line-color": "black",
-                "line-width": 2
+                "line-color": "green",
+                "line-opacity": 0.4,
+                "line-width": 3
+            },
+        });
+        map.addLayer({
+            id: "bike_lanes_layer_cambridge",
+            type: "line", // one of the supported layer types, e.g. line, circle, etc.
+            source: "cambridge_route", // The id we specified in `addSource()`
+            paint: {
+                "line-color": "green",
+                "line-opacity": 0.4,
+                "line-width": 3
             },
         });
     }
