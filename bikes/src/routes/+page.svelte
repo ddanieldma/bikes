288fe37e0a81e9b76af1984fd866d720365b3416
ddanieldma <danielalmemiranda@gmail.com>
2025-05-15 12:33:41 -0300

    drawing bike lanes in the map

--- bikes/src/routes/+page.svelte
+++ bikes/src/routes/+page.svelte
@@ -9,21 +9,44 @@
 
     // --- Variables ---
     let map
-
+    
+    // --- Initiating the map and adding bike lanes data ---
     async function initMap() {
         map = new mapboxgl.Map({
             container: 'map', //binding to div
             style: 'mapbox://styles/mapbox/streets-v12',
             zoom: 12,
             center: [-71.0788727679991, 42.36199480654666]
-        })
+        });
 
-        await new Promise(resolve => map.on("load", resolve))
+        await new Promise(resolve => map.on("load", resolve));
 
-        map.addSource("boston_route", {
-            type: "geojson",
-            data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D",
-        })
+        try {
+            const response = await fetch("/data/Existing_Bike_Network_2022.geojson"); // Adjust the path if your file name is different
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const bikeLaneData = await response.json();
+
+            console.log("bikelanedata", bikeLaneData)
+
+            map.addSource("boston_route", {
+                type: "geojson",
+                data: bikeLaneData,
+            });
+        } catch (error) {
+            console.error("Error loading local bike lane data:", error);
+        }
+
+        map.addLayer({
+            id: "bike_lanes_layer",
+            type: "line", // one of the supported layer types, e.g. line, circle, etc.
+            source: "boston_route", // The id we specified in `addSource()`
+            paint: {
+                "line-color": "black",
+                "line-width": 2
+            },
+        });
     }
     
     // onMount
@@ -31,7 +54,6 @@
         initMap()
     })
     
-
 </script>
 
 <div id="map">
