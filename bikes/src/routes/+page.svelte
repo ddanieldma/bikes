32724b038c27b1969901637adc24fbb19bc3b49c
ddanieldma <danielalmemiranda@gmail.com>
2025-05-15 12:10:26 -0300

    importing and adding bike lane data

--- bikes/src/routes/+page.svelte
+++ bikes/src/routes/+page.svelte
@@ -7,15 +7,31 @@
     // --- Settin mapbox up ---
     mapboxgl.accessToken = "pk.eyJ1IjoiZGRhbmllbGRtYSIsImEiOiJjbWFwaGI3a2MwaWYzMmlvcW5oY2RvanpkIn0.LK2q7zGz4xf741-KEr_CQw"
 
-    // onMount
-    onMount(() => {
-        let map = new mapboxgl.Map({
+    // --- Variables ---
+    let map
+
+    async function initMap() {
+        map = new mapboxgl.Map({
             container: 'map', //binding to div
             style: 'mapbox://styles/mapbox/streets-v12',
             zoom: 12,
             center: [-71.0788727679991, 42.36199480654666]
         })
+
+        await new Promise(resolve => map.on("load", resolve))
+
+        map.addSource("boston_route", {
+            type: "geojson",
+            data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D",
+        })
+    }
+    
+    // onMount
+    onMount(() => {
+        initMap()
     })
+    
+
 </script>
 
 <div id="map">
